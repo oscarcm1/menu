527d337e86a346c85db8ce9ad376b5f29dbe200c
oscar <oscar.delfos369.com>
2023-11-08 05:33:07 -0600

    changes Data Json example

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import { data } from './data/Data'
 
 function App() {
 
   return (
-    <> 
-    
-    </>
+    <div className='box'>
+      
+      {data.map((key) =>
+        <div className='card'  key={key.id} >
+           <img src={key.img} alt=""/>
+           <p>{key.title}</p>
+        </div>
+      )}
+
+    </div>
   )
 }
 
---
+++ src/data/Data.jsx
@@ -0,0 +1,60 @@
+import A from "../assets/a.png"
+import B from "../assets/b.png"
+import C from "../assets/c.png"
+
+export const data = [
+    {
+        id: 1,
+        title: 'Bulbasaur',
+        description: 'Description',
+        img: A
+    },
+    {
+        id: 2,
+        title: 'Squirtle',
+        description: 'Description',
+        img: B
+    },
+    {
+        id: 3,
+        title: 'Pikachu',
+        description: 'Description',
+        img: C
+    },
+    {
+        id: 4,
+        title: 'Bulbasaur',
+        description: 'Description',
+        img: A
+    },
+    {
+        id: 5,
+        title: 'Squirtle',
+        description: 'Description',
+        img: B
+    },
+    {
+        id: 6,
+        title: 'Pikachu',
+        description: 'Description',
+        img: C
+    },
+    {
+        id: 7,
+        title: 'Bulbasaur',
+        description: 'Description',
+        img: A
+    },
+    {
+        id: 8,
+        title: 'Squirtle',
+        description: 'Description',
+        img: B
+    },
+    {
+        id: 9,
+        title: 'Pikachu',
+        description: 'Description',
+        img: C
+    }
+];
