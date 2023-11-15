1d2932d630906c6556cab11340a2e15f7718e578
oscar <oscar.delfos369.com>
2023-11-15 09:28:26 -0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,55 @@ function App() {
 
   return (
     <div className='box'>
-      
-      {data.map((key) =>
+
+      {/* {data.map((key) =>
         <div className='card'  key={key.id} >
            <img src={key.img} alt=""/>
            <p>{key.title}</p>
         </div>
-      )}
+      )} */}
 
+        <div className='card'>
+          <div className='child'>
+          </div>
+           <div className='ok'>
+              <img src={data[0].img} alt=""/>   
+           </div>
+
+           <div className='description'>
+             <p className='title'>{data[0].title}  {" "} N.º 0001
+             </p>
+             <p>Este Pokémon nace con una semilla en el lomo, que brota con el paso del tiempo.</p>
+           </div>
+        </div>
+        
+        <div className='card'>
+          <div className='child'>
+          </div>
+           <div className='ok'>
+              <img src={data[0].img} alt=""/>   
+           </div>
+
+           <div className='description'>
+             <p className='title'>{data[0].title}  {" "} N.º 0001
+             </p>
+             <p>Este Pokémon nace con una semilla en el lomo, que brota con el paso del tiempo.</p>
+           </div>
+        </div>
+        
+        <div className='card'>
+          <div className='child'>
+          </div>
+           <div className='ok'>
+              <img src={data[0].img} alt=""/>   
+           </div>
+
+           <div className='description'>
+             <p className='title'>{data[0].title}  {" "} N.º 0001
+             </p>
+             <p>Este Pokémon nace con una semilla en el lomo, que brota con el paso del tiempo.</p>
+           </div>
+        </div>
     </div>
   )
 }
--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -10,51 +10,52 @@ export const data = [
         img: A
     },
     {
-        id: 2,
-        title: 'Squirtle',
-        description: 'Description',
-        img: B
-    },
-    {
-        id: 3,
-        title: 'Pikachu',
-        description: 'Description',
-        img: C
-    },
-    {
-        id: 4,
-        title: 'Bulbasaur',
-        description: 'Description',
-        img: A
-    },
-    {
-        id: 5,
-        title: 'Squirtle',
-        description: 'Description',
-        img: B
-    },
-    {
-        id: 6,
-        title: 'Pikachu',
-        description: 'Description',
-        img: C
-    },
-    {
-        id: 7,
-        title: 'Bulbasaur',
-        description: 'Description',
-        img: A
-    },
-    {
-        id: 8,
-        title: 'Squirtle',
-        description: 'Description',
-        img: B
-    },
-    {
-        id: 9,
-        title: 'Pikachu',
-        description: 'Description',
-        img: C
-    }
+            id: 2,
+            title: 'Squirtle',
+            description: 'Description',
+            img: B
+        },
+        {
+            id: 3,
+            title: 'Pikachu',
+            description: 'Description',
+            img: C
+        },
+    // 
+    // {
+    //     id: 4,
+    //     title: 'Bulbasaur',
+    //     description: 'Description',
+    //     img: A
+    // },
+    // {
+    //     id: 5,
+    //     title: 'Squirtle',
+    //     description: 'Description',
+    //     img: B
+    // },
+    // {
+    //     id: 6,
+    //     title: 'Pikachu',
+    //     description: 'Description',
+    //     img: C
+    // },
+    // {
+    //     id: 7,
+    //     title: 'Bulbasaur',
+    //     description: 'Description',
+    //     img: A
+    // },
+    // {
+    //     id: 8,
+    //     title: 'Squirtle',
+    //     description: 'Description',
+    //     img: B
+    // },
+    // {
+    //     id: 9,
+    //     title: 'Pikachu',
+    //     description: 'Description',
+    //     img: C
+    // }
 ];
